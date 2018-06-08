d9cb9228411c7017fbb7b5ac03e60548aeafa5dc
Nicholas Arduini <nicholas.arduini@bell.ca>
2018-06-08 08:32:54 -0400

    Working upload endpoint

--- SpeedServer/app.js
+++ SpeedServer/app.js
@@ -1,19 +1,25 @@
 var express = require('express');
 var busboy = require('connect-busboy');
 var config = require('./config');
+var gcloud = require('google-cloud');
+var path = require('path');
+var formidable = require('formidable');
+var fs = require('fs');
 
 var app = express();
 app.use(express.static('public'));
 app.use(busboy());
 app.enable('trust proxy');
 
-var gcloud = require('google-cloud');
-
 var storage = gcloud.storage();
 
 var bucket = storage.bucket(config.bucketName);
 
 var remoteFile = bucket.file('file_200_MB.bin');
+
+app.get('/', function(req, res) {
+  res.send('Testing test...');
+});
 
 app.get('/download', function(req, res) {
   remoteFile.createReadStream()
@@ -23,13 +29,20 @@ app.get('/download', function(req, res) {
     .pipe(res);
 });
 
-app.post("/upload", function(req, res) {
-  if(req.busboy) {
-    req.busboy.on("file", function(fieldName, fileStream, fileName, encoding, mimeType) {
-      res.send('File uploaded');
-    });
-    return req.pipe(req.busboy);
-  }
+app.post('/upload', function(req, res){
+  var form = new formidable.IncomingForm();
+  form.maxFileSize = 1024 * 1024 * 1024;
+  
+  form.on('error', function(err) {
+    console.log('An error has occured: \n' + err);
+  });
+
+  form.on('end', function() {
+    res.end('Upload complete');
+  });
+
+  form.parse(req);
+
 });
 
 app.listen(8080);
--- SpeedServer/k8s-start.sh
+++ SpeedServer/k8s-start.sh
@@ -1,5 +1,5 @@
-docker build -t gcr.io/axiomatic-lamp-176915/speed-node:1.1 .
+docker build -t gcr.io/axiomatic-lamp-176915/speed-node:2.2 .
 
-gcloud docker -- push gcr.io/axiomatic-lamp-176915/speed-node:1.1
+gcloud docker -- push gcr.io/axiomatic-lamp-176915/speed-node:2.2
 
-kubectl set image deployment/speed-node speed-node=gcr.io/axiomatic-lamp-176915/speed-node:1.1
+kubectl set image deployment/speed-node speed-node=gcr.io/axiomatic-lamp-176915/speed-node:2.2
